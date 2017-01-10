07068f3c5e20351a5fdf952a582c89621b8bc5c8
lilvina <davinaleo2007@yahoo.com>
2017-01-10 08:47:55 -0800

    forgot to save

--- clean-racer/database/db.js
+++ clean-racer/database/db.js
@@ -1,7 +1,31 @@
+var pgp = require('pg-promise')()
+var db = pgp(connection);
+
+function getAllLists(req, res, next){
+	return db.many('SELECT * FROM list')
+}
+
+// var createList = 'INSERT INTO list '
+function createList(name){
+	return db.many(
+		`INSERT INTO list(id, description) 
+		VALUES($1, $2)`
+	)
+}
+
+function deleteList(){
+	return db.many(
+		`DELETE FROM
+		list(id, description) 
+		VALUES($1, $2)`
+	)
+}
+
+
 // to create list name
 INSERT INTO list(id,description)
-VALUES(2,'Shopping');
+VALUES(2,'Shopping')
 
 // to delete whole list name
 DELETE FROM list(id,description)
-VALUES(2,'Shopping');
+VALUES(2,'Shopping')
